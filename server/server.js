cc0ba52c8868451199d96633787c13124428c58e
hunksy <vfndtqxtvfuby@mail.ru>
2026-03-27 04:01:02 +0500

    Фикс почты

--- server/server.js
+++ server/server.js
@@ -16,13 +16,12 @@ const emailTransporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST || 'smtp.mail.ru',
     port: parseInt(process.env.EMAIL_PORT) || 465,
     secure: true,
-    family: 4,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
-    tls: {
-        rejectUnauthorized: false
+    lookup: (hostname, options, callback) => {
+        return dns.lookup(hostname, { family: 4 }, callback);
     }
 });
 
